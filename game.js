fb3c713211c6c8177b407e1b58c41880958f92b8
Sarah McDaniel <sarah.mcdaniel@tufts.edu>
2013-08-30 16:53:23 -0400

    finished preliminary animation of first block

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 var xPos;
 var yPos;
+var xLen;
+var currId;
 
 function gameLoop(){
 	console.log("1");
@@ -11,25 +13,69 @@ function setUp(){
 	console.log("2");
 	xPos = 0;
 	yPos = 0;
+	xLen = 72;
 }
 
 function animLoop(){
-	console.log("3");
-	canvas = document.getElementById('game');
+	canvas = document.getElementById('simple');
 	ctx = canvas.getContext('2d');
 	img = new Image();
 	img.src = 'sprites.png';
-	img.onload = function(){]
+	img.onload = function(){
 		setInterval(animate, 140);
-//		window.addEventListener('keydown', userKey, true);
+		window.addEventListener('keydown', userKey, true);
+	}
+}
+
+function userKey(event){
+	switch(event.keyCode){
+	// left arrow
+	case 37:
+		if (xPos > 0){xPos += -24;}
+		break;
+	// right arrow
+	case 39:
+		if (xPos < (240 - xLen)) {xPos += 24;}
+		break;
+	// down arrow
+//	case 40:
+	// up arrow
+	case 38:
+		changePos();
+		break;
+	}
+}
+
+function changePos(){
+	if (currId == 1) {reposA();}
+	if (currId == 2) {reposB();}
+	if (currId == 3) {reposC();}
+	if (currId == 4) {reposD();}
+	if (currId == 5) {reposE();}
+	if (currId == 6) {reposF();}
+	if (currId == 7) {reposG();}
+}
+
+function reposA(){
+	if (currPos == 1){
+		currPos = 2;
+	}
+	else if (currPos == 2){
+		currPos = 3;
+	}
+	else if (currPos == 3){
+		currPos = 4;
+	}
+	else {
+		currPos = 1;
 	}
 }
 
 function animate(){
 	console.log("4");
-//	ctx.clearRect(0, 0, 240, 480);
+	ctx.clearRect(0, 0, 240, 480);
 	draw();
-//	move();
+	move();
 }
 
 function draw(){
@@ -49,5 +95,7 @@ function draw(){
 }
 
 function move(){
-	xPos += 5;
+	console.log("6");
+	if (yPos < 431)
+		yPos += 5;
 }
